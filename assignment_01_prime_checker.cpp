51a9e4754782c8a5869103493f1100eb8958c696
Stephen Anim <sanim021@st.ug.edu.gh>
2026-08-01 15:09:13 +0000

    Array Statistics Calculator

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,7 +36,6 @@
 
 #include <iostream>
 using namespace std;
-
 // Function to check if a number is prime
 bool isPrime(int number)
 {
--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,87 @@
 #include <iostream>
 using namespace std;
 
+// Function to calculate the sum
+int getSum(int arr[], int size)
+{
+    int sum = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+// Function to calculate the average
+double getAverage(int arr[], int size)
+{
+    int sum = getSum(arr, size);
+    return (double)sum / size;
+}
+
+// Function to find the maximum value
+int getMaximum(int arr[], int size)
+{
+    int max = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+// Function to find the minimum value
+int getMinimum(int arr[], int size)
+{
+    int min = arr[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+
+int main()
+{
+    int n;
+
+    cout << "How many numbers? ";
+    cin >> n;
+
+    // Validate input
+    if (n <= 0)
+    {
+        cout << "Error: Number of elements must be greater than 0." << endl;
+        return 0;
+    }
+
+    int arr[n];
+
+    // Read the numbers
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter number " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+
+    // Display results
+    cout << "\nResults:" << endl;
+    cout << "Sum:     " << getSum(arr, n) << endl;
+    cout << "Average: " << getAverage(arr, n) << endl;
+    cout << "Maximum: " << getMaximum(arr, n) << endl;
+    cout << "Minimum: " << getMinimum(arr, n) << endl;
+
+    return 0;
+}
